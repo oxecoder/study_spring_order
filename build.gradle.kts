139f8b37e82917fd8155e5dea3482f6f7337e42a
mp02 <jaehyun.shim@sebasi.co.kr>
2022-07-10 19:52:19 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+  id("org.springframework.boot") version "2.7.1"
+  id("io.spring.dependency-management") version "1.0.11.RELEASE"
+  kotlin("jvm") version "1.6.21"
+  kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-validation") // NotEmpty
+
+  implementation("org.flywaydb:flyway-core:7.10.0")
+//  compileOnly("org.projectlombok:lombok")
+
+    // MapStruct
+    implementation ("org.mapstruct:mapstruct:1.4.2.Final")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.4.2.Final")
+//    annotationProcessor (
+//    "org.projectlombok:lombok",
+//    "org.projectlombok:lombok-mapstruct-binding:0.1.0"
+//  )
+
+  implementation("com.google.guava:guava:28.2-jre")
+  implementation("org.apache.commons:commons-lang3:3.9")
+
+  runtimeOnly("com.h2database:h2")
+  runtimeOnly("mysql:mysql-connector-java")
+  testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "11"
+  }
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/spring_study_order/SpringStudyOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_study_order
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringStudyOrderApplication
+
+fun main(args: Array<String>) {
+  runApplication<SpringStudyOrderApplication>(*args)
+}
